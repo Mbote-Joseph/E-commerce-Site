22f5ba000b52b2e7f7ba43b7e83cb3195c9ca3a4
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-08-27 21:33:50 +0300

    Ecommerce Site in Angular

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   list: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  clicked: boolean = false;
 
   constructor() {}
 
