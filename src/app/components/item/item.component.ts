3f7a332e69103977a6fabe7c9a911a3563037f51
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-08-28 21:15:35 +0300

    Ecommerce Site in Angular

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-item',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
   clicked: boolean = false;
+  @Input() item = 0;
 
   itemClicked(): void {
     this.clicked = true;
