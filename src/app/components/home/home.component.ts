9623b248608b674a4e465ec9b653a1294a4f2c32
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-08-29 22:31:01 +0300

    Ecommerce Site in Angular

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from 'src/app/shared/Item';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   list: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  items: Item[] = [];
   clicked: boolean = false;
   currentItem: number = 0;
 
