9e8c0b8fd8f855b5e8b4a44d9d28d06c99ed74a6
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-08-31 20:53:37 +0300

    Ecommerce Site in Angular

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,7 +8,16 @@ import { Item } from 'src/app/shared/Item';
 })
 export class HomeComponent implements OnInit {
   list: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-  items: Item[] = [];
+  items: Item[] = [
+    {
+      id: 1,
+      name: 'Ter Stegen',
+      description:
+        'Ter Stegen is an open-source project built on top of the world. It is built on top of the world. It is built on top of the world.',
+      price: 78,
+      available: false,
+    },
+  ];
   clicked: boolean = false;
   currentItem: number = 0;
 
