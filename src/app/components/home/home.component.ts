250a5b407a536f6bc049041ef9fc04d0448f64c1
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-09-10 23:18:32 +0300

    Ecommerce Site in Angular

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -163,6 +163,13 @@ export class HomeComponent implements OnInit {
       price: 60,
       available: true,
     },
+    {
+      id: 22,
+      name: 'Joseph Mbote',
+      description: 'The test data description',
+      price: 60,
+      available: true,
+    },
   ];
   clicked: boolean = false;
   currentItem: number = 0;
