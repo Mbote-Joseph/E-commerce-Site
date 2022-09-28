e57b9ff8e7c8fc6e3615e964ccff5612512db58f
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-09-28 22:44:37 +0300

    Ecommerce Site in Angular

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -119,6 +119,13 @@ export class HomeComponent implements OnInit {
     return false;
   }
 
+  isItemAvailable(item: Item): boolean {
+    if (item.available) {
+      return true;
+    }
+    return false;
+  }
+
   deleteItemNew(item: Item): void {
     this.items = this.items.filter((i) => i.id !== item.id);
   }
